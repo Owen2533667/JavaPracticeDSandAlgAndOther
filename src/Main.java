b31b392321322b2143e7194263fb7e3e1e00e5e1
Owen <owenhughes67@gmail.com>
2024-02-11 00:50:59 +0000

    first commit

---
+++ src/Main.java
@@ -0,0 +1,100 @@
+import java.awt.*;
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String s = "Owen Hughes";
+        String t = "HughesOwen";
+
+        System.out.println(isAnagram(s, t));
+
+
+        int[] intArr = {2,3,4,3,5,6,4,5,6};
+        System.out.println(containsDuplicate(intArr));
+        int[] result = twoSum(intArr, 6);
+
+    }
+
+    /*
+    * Contains Duplicate
+    * https://leetcode.com/problems/contains-duplicate/description/
+    * */
+
+    /**
+     * Checks an int array to see if duplicate elements exists. For example, an array [1,2,1,3] will return true as 1 is duplicated.
+     * This is achieved by using a HashSet of Integers, and a for-loop through the array passed by checking if the
+     * HashSet
+     * contains the number, if not then add the number to the set. Otherwise, return true indicating that the number
+     * is a
+     * duplicate. When there is no duplicates the for loop will end and return false.
+     * @param nums int array to be checked.
+     * @return true if array contains duplicate elements, otherwise false.
+     */
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+
+    /**
+     *  Find two numbers, when added together will result in the target number.
+     *  The two numbers, that result in the target, has to be in different index to count.
+     * @param nums array of numbers.
+     * @param target the target which two numbers added in the array will equal.
+     * @return the indices of the two numbers as an array.
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return nums;
+    }
+
+    //first attempt
+    public static boolean isAnagram2(String s, String t) {
+        for (int i = 0; i < t.length(); i++) {
+            if(s.indexOf(t.charAt(i)) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Checks to see if String t is an anagram of String s. Will ignore case sensitivity and space, as the
+     * method will replace spaces and convert two arguments passed to lower case. It then checks to see if length of the
+     * two strings are the same. It will convert the two strings to a char array and sort them. If the two arrays are
+     * equal, it will return true, otherwise false.
+     * @param s first String that t will be checked to.
+     * @param t second String that will be checked if it is an anagram of s.
+     * @return true if t is an anagram of s, otherwise false.
+     */
+    public static boolean isAnagram(String s, String t) {
+
+        String wordOne = s.toLowerCase().replace(" ", "");
+        String wordTwo = t.toLowerCase().replace(" ", "");
+
+        if(wordOne.length() == wordTwo.length()) {
+            char[] wordOneChars = wordOne.toCharArray();
+            char[] wordTwoChars = wordTwo.toCharArray();
+            Arrays.sort(wordOneChars);
+            Arrays.sort(wordTwoChars);
+            if(Arrays.equals(wordOneChars,wordTwoChars)) return true;
+        }
+
+        return false;
+    }
+}
