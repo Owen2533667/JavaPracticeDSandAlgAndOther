3aa2f8983d46563734923bff0466389ee327d5de
Owen <owenhughes67@gmail.com>
2024-02-19 13:33:02 +0000

    Al = BinarySearch

---
+++ src/binarysearch/Main.java
@@ -0,0 +1,52 @@
+package binarysearch;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Binary search = Search algorithm that finds the position
+        //                 of a target value within a sorted arrays.
+        //                 Half of the array is eliminated during each "step"
+
+        int array[] = new int[1000000];
+        int target = 800787;
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+
+        // int index = Arrays.binarySearch(array, target);
+        int index = binarySearch(array, target);
+
+        if (index == -1) {
+            System.out.println(target + " not found");
+        } else {
+            System.out.println("Element found at: " + index);
+        }
+
+    }
+
+    private static int binarySearch(int[] array, int target) {
+
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            int value = array[middle];
+
+            System.out.println("**************");
+            System.out.println("low: " + low);
+            System.out.println("middle: " + value);
+            System.out.println("high: " + high);
+
+            if (value < target) low = middle + 1;
+            else if (value > target) high = middle - 1;
+            else return middle; // target found
+        }
+
+        return -1; // target not found
+    }
+
+}
