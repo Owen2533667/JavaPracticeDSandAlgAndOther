03e24cec17f6a475c18f719690ba94dd3508d1bc
Owen <owenhughes67@gmail.com>
2024-02-27 23:11:59 +0000

    Use of functional interfaces

---
+++ src/functionalinterface/GreetingMessage.java
@@ -0,0 +1,8 @@
+package functionalinterface;
+
+@FunctionalInterface
+public interface GreetingMessage {
+
+    public abstract void greet(String name);
+
+}
---
+++ src/functionalinterface/Main.java
@@ -0,0 +1,20 @@
+package functionalinterface;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        GreetingMessage gm = new GreetingMessage() {
+            @Override
+            public void greet(String name) {
+                System.out.println("Hello, " + name);
+            }
+        };
+
+        gm.greet("Owen");
+
+
+    }
+
+}
