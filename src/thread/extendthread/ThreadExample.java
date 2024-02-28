10886ce1d297ecd376acb5d759092be6e63b0762
Owen <owenhughes67@gmail.com>
2024-02-28 01:59:45 +0000

    Java threads

---
+++ src/thread/extendthread/ThreadExample.java
@@ -0,0 +1,15 @@
+package thread.extendthread;
+
+public class ThreadExample extends Thread {
+
+    @Override
+    public void run() {
+        int i = 1;
+        while(i <= 100) {
+            System.out.println(i + " " + this.getName());
+            i++;
+        }
+    }
+
+
+}
---
+++ src/thread/runnable/RunnableExample.java
@@ -0,0 +1,16 @@
+package thread.runnable;
+
+public class RunnableExample implements Runnable {
+
+    @Override
+    public void run() {
+        int i = 0;
+        while (i <= 100) {
+            System.out.println(i + " " + Thread.currentThread().getName());
+            i++;
+        }
+
+    }
+
+
+}
