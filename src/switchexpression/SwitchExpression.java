b462bdba5cb0d29c78cfbc48517b8a79ef3ceb78
Owen <owenhughes67@gmail.com>
2024-02-29 10:57:19 +0000

    switch expression

---
+++ src/switchexpression/SwitchExpression.java
@@ -0,0 +1,19 @@
+package switchexpression;
+
+public class SwitchExpression {
+
+    public static void main(String[] args) {
+
+        int dayOfWeek = 3; // Example: Wednesday
+        int numLetters = switch (dayOfWeek) {
+            case 1, 5, 7 -> 6; // Monday, Friday, Sunday
+            case 2 -> 7; // Tuesday
+            case 4, 6 -> 8; // Thursday, Saturday
+            case 3 -> 9; // Wednesday
+            default -> throw new IllegalStateException("Invalid day: " + dayOfWeek);
+        };
+        System.out.println("Number of letters: " + numLetters);
+
+    }
+
+}
